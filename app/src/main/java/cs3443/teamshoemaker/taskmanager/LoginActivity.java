0f681ecea7a3892343796cdf8a098a74dda2305e
lidiads22 <Lidiads22@icloud.com>
2023-07-13 00:40:31 -0500

    test

---
+++ app/src/main/java/cs3443/teamshoemaker/taskmanager/LoginActivity.java
@@ -0,0 +1,18 @@
+package cs3443.teamshoemaker.taskmanager;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Add your login functionality here
+        // e.g., handle login button, authenticate user, etc.
+    }
+}
+
